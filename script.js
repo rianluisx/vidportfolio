be584efcb164dce9984bb820d0430deffc22a115
rianluisx <rcmabait@up.edu.ph>
2024-06-09 02:10:22 +0800

    minor changes

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-
-//projects
-
 const videos = document.querySelector(".videos");
 const videoplayer = document.querySelector(".videos .videoplayer");
 const close = document.querySelector(".videos .close");
@@ -11,10 +8,10 @@ const playVideo = (googleDriveLink) => {
     
     let width, height;
     
-    // Define different resolutions based on media queries
+
     if (window.matchMedia("(max-width: 550px)").matches) {
-        width = 320; // Adjust width for smaller screens
-        height = 180; // Adjust height for smaller screens
+        width = 320; 
+        height = 180; 
     } else if (window.matchMedia("(max-width: 800px)").matches) {
         width = 450;
         height = 320;
@@ -22,8 +19,8 @@ const playVideo = (googleDriveLink) => {
         width = 820;
         height = 450;
     } else {
-        width = 1280; // Default width for larger screens
-        height = 720; // Default height for larger screens
+        width = 1280; 
+        height = 720; 
     }
     
     const embeddableUrl = `https://drive.google.com/file/d/${videoId}/preview`;
@@ -34,10 +31,10 @@ const playVideo = (googleDriveLink) => {
 
 close.addEventListener("click", () => {
     videos.classList.remove("active");
-    videoplayer.innerHTML = ''; // Remove the iframe content
+    videoplayer.innerHTML = ''; 
 });
 
-// Function to extract Google Drive video ID from a link
+
 function extractGoogleDriveVideoId(link) {
     const regex = /\/file\/d\/([a-zA-Z0-9_-]+)\//;
     const match = link.match(regex);
@@ -49,7 +46,7 @@ function extractGoogleDriveVideoId(link) {
     }
 }
 
-//about script
+
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("content");
 
@@ -66,7 +63,7 @@ function opentab(tabname){
     document.getElementById(tabname).classList.add("active-tab");
 }
 
-// sidebar
+
 
 var sidebar = document.getElementById("sidebar");
 
@@ -78,7 +75,7 @@ function closemnu(){
     sidebar.style.right = "-200px";
 }
 
-// contacts 
+
 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyyNzseZolYSz3qDPnCjgZkuJclBYOydmBB2aX1N213NhlBHSAkCz0iQmVJGa4B_SQx/exec'
 const form = document.forms['submit-to-google-sheet']
@@ -97,7 +94,7 @@ form.addEventListener('submit', e => {
     .catch(error => console.error('Error!', error.message))
 })
 
-//sticky navbar
+
 
 const navbar = document.getElementById('navbar');
 
